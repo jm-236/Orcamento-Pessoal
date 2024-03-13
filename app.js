fa2771c58381be1f0cfdd743d792f0a5096ce798
João Marcelo Costa de Santana <159437965+jm-236@users.noreply.github.com>
2024-03-13 15:18:04 -0300

    exibição das despesas na tela de consulta

--- app.js
+++ app.js
@@ -64,7 +64,77 @@ function cadastrarDespesa() {
     document.getElementById("valor").value = ""
 }
 
+function carregaListaDespesas() {
+    let lista = []
+    let i = localStorage.getItem('id')
+
+    while(i > 0){
+        let d = JSON.parse(localStorage.getItem(i))
+        if (d !== null){
+            lista.push(d)
+        }
+        i--
+    }
+
+    var listaDespesas = document.getElementById("listaDespesas")
+
+    lista.forEach(function(d) {
+        // console.log(d)
+        let linha = listaDespesas.insertRow()  
+        
+        linha.insertCell(0).innerHTML = `${d.dia}/${d.mes}/${d.ano}`
+
+        switch(d.tipo){
+            case '1':
+                linha.insertCell(1).innerHTML = 'Alimentação'
+                break
+            case '2':
+                linha.insertCell(1).innerHTML = 'Educação'
+                break
+            case '3':
+                linha.insertCell(1).innerHTML = 'Lazer'
+                break
+            case '4':
+                linha.insertCell(1).innerHTML = 'Saúde'
+                break
+            case '5':
+                linha.insertCell(1).innerHTML = 'Transportes'
+                break
+        }
+
+        linha.insertCell(2).innerHTML = d.descricao
+        linha.insertCell(3).innerHTML = `${parseFloat(d.valor).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}`
+        
+    });
+}
+
+function pesquisarDespesas() {
+    let ano = document.getElementById("ano").value
+    let mes = document.getElementById("mes").value
+    let dia = document.getElementById("dia").value
+    let tipo = document.getElementById("tipo").value 
+    let descricao = document.getElementById("descricao").value
+    let valor = document.getElementById("valor").value
+    let despesa = {
+        ano,
+        mes,
+        dia,
+        tipo,
+        descricao,
+        valor,
+        // validarDados: function() {
+        //     for (let i in this){
+        //         if (this[i] == "" || this[i] == null || this[i] == undefined){
+        //             return false
+        //         }
+        //     }
+        //     return true
+        // }
+    }
+    console.log(despesa)
+}
+
+// main
 if (localStorage.getItem('id') === null){
     localStorage.setItem('id', 0)
 }
-
