29d998e46edf181c9f48142df17cafd4aa7259f3
João Marcelo Costa de Santana <159437965+jm-236@users.noreply.github.com>
2024-03-13 17:17:14 -0300

    Criação da remoção da despesa

--- app.js
+++ app.js
@@ -5,7 +5,19 @@ function getproximoId(){
 }
 
 function gravar(d){
-    localStorage.setItem(getproximoId(), JSON.stringify(d))
+    let id = getproximoId()
+    // d.id = id
+    // console.log(d)
+    localStorage.setItem(id, JSON.stringify(d))
+}
+
+function janelaSucessoRemocao() {
+
+    document.getElementById("div-titulo").className = "modal-header text-success"
+    document.getElementById("div-titulo").innerHTML = "Sucesso na Exclusão!"
+    document.getElementById("texto-aviso").innerHTML = "\n  Despesa removida com sucesso.\n"
+    document.getElementById("botao-confirmacao").className = "btn btn-success"
+    document.getElementById("botao-confirmacao").innerHTML = "Ok"
 }
 
 function janelaSucesso() {
@@ -55,7 +67,7 @@ function cadastrarDespesa() {
         janelaErro()
         $('#modalRegistraDespesa').modal('show')
     }
-    
+
     document.getElementById("ano").value = ""
     document.getElementById("mes").value = ""
     document.getElementById("dia").value = ""
@@ -64,24 +76,33 @@ function cadastrarDespesa() {
     document.getElementById("valor").value = ""
 }
 
-function carregaListaDespesas() {
+function recuperarDados() {
     let lista = []
     let i = localStorage.getItem('id')
 
     while(i > 0){
         let d = JSON.parse(localStorage.getItem(i))
         if (d !== null){
+            d.id = i
             lista.push(d)
         }
         i--
     }
+    return lista
+}
 
+function mostrarListaDespesas(lista = Array(), filtro = false) {
     var listaDespesas = document.getElementById("listaDespesas")
+    listaDespesas.innerHTML = ""
+
+    if (lista.length == 0 && filtro == false) {
+        var lista = recuperarDados()
+    }
 
     lista.forEach(function(d) {
         // console.log(d)
-        let linha = listaDespesas.insertRow()  
-        
+        let linha = listaDespesas.insertRow()
+
         linha.insertCell(0).innerHTML = `${d.dia}/${d.mes}/${d.ano}`
 
         switch(d.tipo){
@@ -104,35 +125,53 @@ function carregaListaDespesas() {
 
         linha.insertCell(2).innerHTML = d.descricao
         linha.insertCell(3).innerHTML = `${parseFloat(d.valor).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}`
-        
+
+        let btn = document.createElement("button")
+        btn.className = 'btn btn-outline-danger'
+        btn.innerHTML = '<i class="fas fa-times"></i>'
+        btn.id = `id_despesa_${d.id}`
+        btn.onclick = function(){
+
+            let id = this.id.replace("id_despesa_", "")
+            localStorage.removeItem(id)
+
+            janelaSucessoRemocao()
+            $('#modalRegistraDespesa').modal('show')
+            // window.location.reload()
+        }
+
+        linha.insertCell(4).append(btn)
+
     });
 }
 
-function pesquisarDespesas() {
-    let ano = document.getElementById("ano").value
-    let mes = document.getElementById("mes").value
-    let dia = document.getElementById("dia").value
-    let tipo = document.getElementById("tipo").value 
-    let descricao = document.getElementById("descricao").value
-    let valor = document.getElementById("valor").value
-    let despesa = {
-        ano,
-        mes,
-        dia,
-        tipo,
-        descricao,
-        valor,
-        // validarDados: function() {
-        //     for (let i in this){
-        //         if (this[i] == "" || this[i] == null || this[i] == undefined){
-        //             return false
-        //         }
-        //     }
-        //     return true
-        // }
-    }
-    console.log(despesa)
+function removerDespesa() {
+
 }
+
+function pesquisarDespesas() {
+
+    let despesa = {
+        ano: document.getElementById("ano").value,
+        mes: document.getElementById("mes").value,
+        dia: document.getElementById("dia").value,
+        tipo: document.getElementById("tipo").value ,
+        descricao: descricao = document.getElementById("descricao").value,
+        valor: valor = document.getElementById("valor").value,
+    }
+
+    let lista = recuperarDados()
+
+    for(i in despesa){
+        // console.log(despesa[i])
+        if (despesa[i] != ""){
+            lista = lista.filter(d => despesa[i] == d[i])
+        }
+    }
+    console.log(lista)
+    mostrarListaDespesas(lista, true)
+}
+
 
 // main
 if (localStorage.getItem('id') === null){
