e2a8dc26eae0e7cf6026b64a42d32b475dc5fde7
João Marcelo Costa de Santana <159437965+jm-236@users.noreply.github.com>
2024-03-13 12:00:08 -0300

    registro de despesas e erros de preenchimento

--- app.js
+++ app.js
@@ -1,18 +1,70 @@
+function getproximoId(){
+    let proximoId = parseInt(localStorage.getItem('id')) + 1
+    localStorage.setItem('id', parseInt(localStorage.getItem('id')) + 1)
+    return proximoId
+}
+
+function gravar(d){
+    localStorage.setItem(getproximoId(), JSON.stringify(d))
+}
+
+function janelaSucesso() {
+
+    document.getElementById("div-titulo").className = "modal-header text-success"
+    document.getElementById("div-titulo").innerHTML = "Sucesso na gravação!"
+    document.getElementById("texto-aviso").innerHTML = "\n  Despesa salva com sucesso.\n"
+    document.getElementById("botao-confirmacao").className = "btn btn-success"
+    document.getElementById("botao-confirmacao").innerHTML = "Ok"
+}
+
+function janelaErro() {
+
+    document.getElementById("div-titulo").className = "modal-header text-danger"
+    document.getElementById("div-titulo").innerHTML = "Erro na gravação!"
+    document.getElementById("texto-aviso").innerHTML = "\n  Preencha todos os dados corretamente.\n"
+    document.getElementById("botao-confirmacao").className = "btn btn-danger"
+    document.getElementById("botao-confirmacao").innerHTML = "Voltar e corrigir"
+}
+
 function cadastrarDespesa() {
+
     let novaDespesa = {
         ano: document.getElementById("ano").value,
         mes: document.getElementById("mes").value,
         dia: document.getElementById("dia").value,
         tipo: document.getElementById("tipo").value,
         descricao: document.getElementById("descricao").value,
-        valor: document.getElementById("valor").value
+        valor: document.getElementById("valor").value,
+
+        validarDados: function() {
+            for (let i in this){
+                if (this[i] == "" || this[i] == null || this[i] == undefined){
+                    return false
+                }
+            }
+            return true
+        }
     }
 
-    // if (novaDespesa.ano == "" || novaDespesa.mes == "" || novaDespesa.dia == ""
-    // || novaDespesa.tipo == "" || novaDespesa.descricao == "" || novaDespesa.valor == ""){ }
-    gravar(novaDespesa)
+    if (novaDespesa.validarDados()){
+        gravar(novaDespesa)
+        janelaSucesso()
+        $('#modalRegistraDespesa').modal('show')
+    }
+    else{
+        janelaErro()
+        $('#modalRegistraDespesa').modal('show')
+    }
+    
+    document.getElementById("ano").value = ""
+    document.getElementById("mes").value = ""
+    document.getElementById("dia").value = ""
+    document.getElementById("tipo").value = ""
+    document.getElementById("descricao").value = ""
+    document.getElementById("valor").value = ""
 }
 
-function gravar(d){
-    localStorage.setItem('Despesa', JSON.stringify(d))
+if (localStorage.getItem('id') === null){
+    localStorage.setItem('id', 0)
 }
+
